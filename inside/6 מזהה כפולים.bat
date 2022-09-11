d3f017921d5f8df88342636bd31a104c87791a00
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2022-09-11 06:28:28 +0300

    עדכון קרוב לסופי

--- inside/6 מזהה כפולים.bat
+++ inside/6 מזהה כפולים.bat
@@ -49,40 +49,41 @@ echo.
 set /a num=1
 for /f "eol=;tokens=1,1*delims=" %%a in (%temp%\list-to-delete.tmp) do (
 set item=%%a
-set viwe_item=%%~na
+set viwe_item="%%~na"
 call :choice-delete
 )
 del "%temp%\list-to-delete.tmp"
 ::הצגת רשימת השירים למחיקה
 echo -------------------
+echo Pres any key to delete file
 :choicer
 choice /c ABCDEFGHIJKLMNOPQRSTUVWXYZ
-if errorlevel 26 echo "%itemZ%" |rev & goto :choicer
-if errorlevel 25 echo "%itemY%" |rev & goto :choicer
-if errorlevel 24 echo "%itemX%" |rev & goto :choicer
-if errorlevel 23 echo "%itemW%" |rev & goto :choicer
-if errorlevel 22 echo "%itemV%" |rev & goto :choicer
-if errorlevel 21 echo "%itemU%" |rev & goto :choicer
-if errorlevel 20 echo "%itemT%" |rev & goto :choicer
-if errorlevel 19 echo "%itemS%" |rev & goto :choicer
-if errorlevel 18 echo "%itemR%" |rev & goto :choicer
-if errorlevel 17 echo "%itemQ%" |rev & goto :choicer
-if errorlevel 16 echo "%itemP%" |rev & goto :choicer
-if errorlevel 15 echo "%itemO%" |rev & goto :choicer
-if errorlevel 14 echo "%itemN%" |rev & goto :choicer
-if errorlevel 13 echo "%itemM%" |rev & goto :choicer
-if errorlevel 12 echo "%itemL%" |rev & goto :choicer
-if errorlevel 11 echo "%itemK%" |rev & goto :choicer
-if errorlevel 10 echo "%itemJ%" |rev & goto :choicer
-if errorlevel 9 echo "%itemI%" |rev & goto :choicer
-if errorlevel 8 echo "%itemH%" |rev & goto :choicer
-if errorlevel 7 echo "%itemG%" |rev & goto :choicer
-if errorlevel 6 echo "%itemF%" |rev & goto :choicer
-if errorlevel 5 echo "%itemE%" |rev & goto :choicer
-if errorlevel 4 echo "%itemD%" |rev & goto :choicer
-if errorlevel 3 echo "%itemC%" |rev & goto :choicer
-if errorlevel 2 echo "%itemB%" |rev & goto :choicer
-if errorlevel 1 echo "%itemA%" |rev & goto :choicer
+if errorlevel 26 del "%itemZ%" & goto :choicer
+if errorlevel 25 del "%itemY%" & goto :choicer
+if errorlevel 24 del "%itemX%" & goto :choicer
+if errorlevel 23 del "%itemW%" & goto :choicer
+if errorlevel 22 del "%itemV%" & goto :choicer
+if errorlevel 21 del "%itemU%" & goto :choicer
+if errorlevel 20 del "%itemT%" & goto :choicer
+if errorlevel 19 del "%itemS%" & goto :choicer
+if errorlevel 18 del "%itemR%" & goto :choicer
+if errorlevel 17 del "%itemQ%" & goto :choicer
+if errorlevel 16 del "%itemP%" & goto :choicer
+if errorlevel 15 del "%itemO%" & goto :choicer
+if errorlevel 14 del "%itemN%" & goto :choicer
+if errorlevel 13 del "%itemM%" & goto :choicer
+if errorlevel 12 del "%itemL%" & goto :choicer
+if errorlevel 11 del "%itemK%" & goto :choicer
+if errorlevel 10 del "%itemJ%" & goto :choicer
+if errorlevel 9 del "%itemI%" & goto :choicer
+if errorlevel 8 del "%itemH%" & goto :choicer
+if errorlevel 7 del "%itemG%" & goto :choicer
+if errorlevel 6 del "%itemF%" & goto :choicer
+if errorlevel 5 del "%itemE%" & goto :choicer
+if errorlevel 4 del "%itemD%" & goto :choicer
+if errorlevel 3 del "%itemC%" & goto :choicer
+if errorlevel 2 del "%itemB%" & goto :choicer
+if errorlevel 1 del "%itemA%" & goto :choicer
 
 
 pause
@@ -117,7 +118,7 @@ if %num%==23 set let=X
 if %num%==24 set let=Y
 if %num%==25 set let=Z
 echo %viwe_item% ]%let%[|rev
-set item%let%=%viwe_item%
+set item%let%=%item%
 set /a num=num+1
 exit /b
 
