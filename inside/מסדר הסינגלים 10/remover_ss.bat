4a02922808907d655e5d775e73da8e462d659467
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2022-11-17 20:57:16 +0200

    עדכון

--- inside/מסדר הסינגלים 10/remover_ss.bat
+++ inside/מסדר הסינגלים 10/remover_ss.bat
@@ -12,8 +12,6 @@ del "%AppData%\Microsoft\Windows\Start Menu\Programs\מסדר הסינגלים\מסדר הסינגלים
 ::מחיקת ערך הרישום האחראי על רשימת התוכנות להסרה
 reg delete "HKEY_LOCAL_MACHINE\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\SinglesSorter" /f
 
-
-
 ::מחיקת קבצי התוכנה
 for %%i in ("%appdata%\singles-sorter\*.*") do (if not "%%~nxi"=="remover_ss.bat" del "%%~i")
 del %0%
