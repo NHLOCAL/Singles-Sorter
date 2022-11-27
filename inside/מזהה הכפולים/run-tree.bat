dcc4779d7b8a74bf9f7bd511a8e387540eb7e95a
אורי פפר <NH.LOCAL11@GMAIL.COM>
2022-11-27 17:54:01 +0200

    delete for new repo

--- inside/מזהה הכפולים/run-tree.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-chcp 1255
-FOR /d %%i in ("J:\שמע\כל המוזיקה\*") do call "C:\Users\אורי\Documents\GitHub\Singles-Sorter\inside\מזהה כפולים\Duplicatedetectiontree.bat" "%%~i" && cd "J:\שמע\כל המוזיקה"
-
-pause
--- inside/מזהה הכפולים/הרץ פייתון על עץ תיקיות.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-chcp 1255
-
-for /d %%i in ("J:\שמע\כל המוזיקה\*") do "C:\Users\אורי\Documents\GitHub\Singles-Sorter\inside\מזהה הכפולים\Find_duplic_ample.py" "%%~i\סינגלים"
-
-pause
