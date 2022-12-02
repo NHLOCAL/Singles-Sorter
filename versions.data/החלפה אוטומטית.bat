92d3118f00eab79b724729f97813cc0377b1ced5
אורי פפר <NH.LOCAL11@GMAIL.COM>
2022-12-02 03:49:01 +0200

    עדכונים עבור הגרסה החדשה

--- versions.data/החלפה אוטומטית.bat
+++ versions.data/החלפה אוטומטית.bat
@@ -1,2 +1,5 @@
 md "folder"
-for %%i in (*ver*) do (powershell -Command "(gc %%i) -replace '9.0', '9.1' | Out-File -encoding default folder\%%i")
+set old=9.1
+set new=9.2
+copy "%old%+version" "%new%+version"
+for %%i in (*ver*) do (powershell -Command "(gc %%i) -replace '%old%', '%new%' | Out-File -encoding default folder\%%i")
