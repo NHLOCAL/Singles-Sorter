b2a19832cc12725e67a6b0af75b77b373229583e
אורי פפר <NH.LOCAL11@GMAIL.COM>
2022-12-04 19:17:04 +0200

    Update pro_func.py

--- pro-func/pro_func.py
+++ pro-func/pro_func.py
@@ -1,18 +1,24 @@
 # -*- coding: utf-8 -*-
 import os
 import sys
+
 # יבוא פונצקיה לקריאת מטאדאטה של קובץ
 import music_tag
-# יבוא פונקציה לקריאת עץ תיקיות
+
+# יבוא פונצקיה לקריאת עץ תיקיות
 from os.path import join, getsize
+
 # יבוא פונצקיה עבור תצוגת האותיות העבריות
 from bidi.algorithm import get_display
-   
 
 
 def pro_scanner(my_file, root):
-# הפונקציה סורקת את המטאדאטה של השיר ומכניסה אותו למשתנה
+    """
+    פונקציית סורקת את המטאדאטה של השיר ומכניסה אותו למשתנה
+    """
     try:
+        if not my_file.endswith(".mp3"):
+            return
         my_file = root + "\\" + my_file
         artist_file = music_tag.load_file(my_file)
         artist = artist_file['artist']
@@ -20,17 +26,16 @@ def pro_scanner(my_file, root):
     except:
         pass
 
-   
+
 def main():
     global target_dict
     target_dict = {}
     dir_path = str(sys.argv[1])
     if (dir_path != "") and (os.path.exists(dir_path)):
         for root, dirs, files in os.walk(dir_path):
-            #print("{}".format(root))
-            #continue
             for my_file in files:
                 pro_scanner(my_file, root)
+                
         dict_list = target_dict.items()          
         for item in dict_list:
             file_name = str(item[0])
@@ -38,8 +43,7 @@ def main():
             if art_name.isdigit() or art_name.isalpha() or "&" in art_name or art_name == "" or "," in art_name or len(art_name.split()) >= 3:
                 continue
             print("move " + file_name + " to " + art_name)
-            
-    os.system('pause')
+
 
 if __name__ == '__main__':
     main()
