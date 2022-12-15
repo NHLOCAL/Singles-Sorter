fca4694abe8dd3b02e789bd97aa3fa196a74a9ad
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2022-12-16 00:33:07 +0200

    שחרור גרסה 10 - פירוט על הגרסה החדשה

--- versions.data/החלפה אוטומטית.bat
+++ versions.data/החלפה אוטומטית.bat
@@ -1,5 +1,5 @@
 md "folder"
-set old=9.1
-set new=9.2
+set old=9.2
+set new=10.0
 copy "%old%+version" "%new%+version"
 for %%i in (*ver*) do (powershell -Command "(gc %%i) -replace '%old%', '%new%' | Out-File -encoding default folder\%%i")
