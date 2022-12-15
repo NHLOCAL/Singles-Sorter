b8ef0e0869156fa84edd02e764e6bf1d166b46b9
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2022-12-15 22:11:11 +0200

    Create up_to_10.bat

---
+++ inside/מסדר הסינגלים 10/up_to_10.bat
@@ -0,0 +1,14 @@
+@echo off
+chcp 1255
+color f1
+title עדכון מסדר הסינגלים
+MODE CON COLS=80 lines=27
+
+echo.
+echo                    הווצא ץבוקכ רתוי ןימז וניא םילגניסה רדסמ
+echo                         !תבצועמהו השדחה הסרגה דרת דימ
+echo.
+start https://nhlocal.github.io/Singles-Sorter/site/download.html
+
+
+pause
