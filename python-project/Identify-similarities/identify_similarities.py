1a33ac60e1b1cc22f2dd5c0eabc92ea2c16d0d4b
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2022-12-20 02:19:37 +0200

    הוספת תת פונקציה להחזרת אמת או שקר

--- python-project/Identify-similarities/identify_similarities.py
+++ python-project/Identify-similarities/identify_similarities.py
@@ -2,11 +2,17 @@ from difflib import SequenceMatcher
 
 def find_text_similarity(text, text_list):
     """
-    בדיקת דמיון בין מחרוזת מסויימת לרשימת מחרוזות
+בדיקת דמיון בין מחרוזת מסויימת לרשימת מחרוזות
     
-    פרמטרים:
+פרמטרים:
     פרמטר 1 = מחרוזת טקסט
     פרמטר 2 = רשימת מחרוזות טקסט
+
+תוצאה:
+    טאפל עם 3 משתנים:
+    אייטם 1 - שם השיר הדומה ביותר
+    אייטם 2 - מספר המייצג את רמת הדמיון
+    אייטם 3 - רשימה של כל הפריטים עם רמת הדמיון שלהם לפריט היחיד
     """
     # יצירת רשימת דמיונים
     similarity_list = []
@@ -36,10 +42,32 @@ def find_text_similarity(text, text_list):
 
     return most_similar_string, max_similarity, sum_list
 
+
+# הפונקציה מקבלת ערכי מספרים מפונקציית "find_text_similarity"
+# וממירה אותם לאמת או שקר בהתאם לרמת הדמיון בין המחרוזות
+# ובהתאמה לאורך המחרוזות
+def true_or_false(text, text_list):
+    
+    # הפעלת הפונקציה לזיהוי דמיון בין מחרוזות וקבלת ערכי משתנים
+    most_similar_string, max_similarity, sum_list = find_text_similarity(text, text_list)
+    
+    # ניתוח ערכי המשתנים בהתאם לאורך המחרוזת
+    str_len = len(text)
+    
+    # הגדרת רמת ההתאמה הנדרשת בתרגיל חשבוני
+    Required_level_similarity = 1.0 - (str_len * 0.01)
+    
+    # אם רמת הדמיון מספקת, החזר אמת, אם לא החזר שקר
+    if max_similarity > Required_level_similarity:
+        return Required_level_similarity, max_similarity, True
+    else:
+        return False
+
+
 def main():
     text = "אברימי רוט"
     text_list = ["אבריימי רוט", "אברימי לוי", "אברהם פריד", "מוטי שטיינמץ", "אברמי רוט", "אברימי רוט"]
-    most_similar_string, max_similarity,  sum_list = find_text_similarity(text, text_list)
+    most_similar_string, max_similarity, sum_list = find_text_similarity(text, text_list)
     print(f'השם הדומה ביותר למחרוזת הראשונה הוא "{most_similar_string}" עם דמיון של {max_similarity:.2f}')
     print("-" * 70)
     for i in sum_list:
@@ -47,5 +75,10 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    text = "אברימי רוט"
+    text_list = ["אברימי לוי", "אברהם פריד", "מוטי שטיינמץ"]
+    
+    a = true_or_false(text, text_list)
+    print(a)
+    #main()
 
