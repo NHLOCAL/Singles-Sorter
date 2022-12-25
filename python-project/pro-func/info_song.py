04506e16c2194ce1f119f9e50ae6f780a817b58c
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2022-12-26 01:12:30 +0200

    שדרוג עבור שחרור

--- python-project/pro-func/info_song.py
+++ python-project/pro-func/info_song.py
@@ -4,16 +4,43 @@ import sys
 
 def main():
     try:
-        filepath = str(sys.argv[1])
-        artist = artist_from_song(filepath)
+        file_path = str(sys.argv[1])
+        target_path = str(sys.argv[2])
+        arg_list = []
+        for arg in sys.argv:
+            arg = str(arg)
+            arg_list.append(arg)
+        arg_list = arg_list[3::]
+
+        arg_list.sort(key=sort_arg)
+        print(arg_list)
+        '''
+        artist = artist_from_song(arg_list)
         checking = check_artist(artist)
         if checking:
             print(artist)
         else:
             print(False)
-    except:
+        '''
+    except Exception as e:
         print("הכנס נתיב שיר לקבלת שם האמן שלו")
+
+
+def sort_arg(arg):
+    # ארגומנט יצירת תיקית סינגלים
+    if arg == "s-f:yes" or arg == "s-f:no": return 1
+    # ארגומנט יצירת תיקיות א' ב'
+    elif arg == "ab-f:yes" or arg == "ab-f:no": return 2
+    # ארגומנט העתקה או העברה
+    elif arg == "copy" or arg == "move": return 3
+    # ארגומנט העברה לתיקיה קיימת בלבד
+    elif arg == "o-exist:yes" or arg == "o-exist:no": return 4
+    # ארגומנט מיון תיקיה ראשית בלבד
+    elif arg == "o-main:yes" or arg == "o-main:no": return 5
+    else: return False
 
 
 if __name__ == '__main__':
     main()
+    
+    
