c184d0180cbf66bb3ef70fe149c8b4048258ff4f
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2023-06-04 05:27:37 +0300

    התאמת הפונקציה למחרוזות מעורבות

--- python-project/jibrish_to_hebrew.py
+++ python-project/jibrish_to_hebrew.py
@@ -7,45 +7,50 @@ def jibrish_to_hebrew(string):
 פרמטר 1 = מחרוזת בעברית משובשת
     """
 
-    # הגדרת מילונים להמרה מעברית לגי'בריש ולהפך
     heb_to_jib = {'א': 'à', 'ב': 'á', 'ג': 'â', 'ד': 'ã', 'ה': 'ä', 'ו': 'å', 'ז': 'æ', 'ח': 'ç', 'ט': 'è', 'י': 'é', 'כ': 'ë', 'ל': 'ì', 'מ': 'î','נ': 'ð', 'ס': 'ñ', 'ע': 'ò', 'פ': 'ô', 'צ': 'ö', 'ק': '÷', 'ר': 'ø', 'ש': 'ù', 'ת': 'ú', 'ך': 'ê', 'ם': 'í', 'ן': 'ï', 'ף': 'ó', 'ץ': 'õ'}
     jib_to_heb = {'à': 'א', 'á': 'ב', 'â': 'ג', 'ã': 'ד', 'ä': 'ה', 'å': 'ו', 'æ': 'ז', 'ç': 'ח', 'è': 'ט', 'é': 'י', 'ë': 'כ', 'ì': 'ל', 'î': 'מ', 'ð': 'נ', 'ñ': 'ס', 'ò': 'ע', 'ô': 'פ', 'ö': 'צ', '÷': 'ק', 'ø': 'ר', 'ù': 'ש', 'ú': 'ת', 'ê': 'ך', 'í': 'ם', 'ï': 'ן', 'ó': 'ף', 'õ': 'ץ'}
 
-    # הגדרת מחרוזת ריקה למילוי אוטומטי בהמשך
     new_string = ""
+    setting = None
     
-    # בדיקה של סוג המחרוזת
-    if any(c in "אבגדהוזחטיכלמנסעפצקרשתךםןףץ" for c in string):
-        setting = "jib"
-    elif any(c in "àáâãäåæçèéëìîðñòôö÷øùúêíïóõ" for c in string):
-        setting = "heb"
-        
-    # מעבר על המחרוזת שהוזנה והמרה שלה, אות אחר אות
     for letter in string:
-        # דילוג אם האות הנוכחית היא רווח
-        if letter == " ":
-            new_string = new_string + " "
-        elif setting == "heb":
-            try:
+        if setting == "heb":
+            if letter in jib_to_heb:
                 new_letter = jib_to_heb[letter]
-                new_string = new_string + new_letter
-            except:
-                pass
+                new_string += new_letter
+            else:
+                new_string += letter
         elif setting == "jib":
-            try:
+            if letter in heb_to_jib:
                 new_letter = heb_to_jib[letter]
-                new_string = new_string + new_letter
-            except:
-                pass
-            
+                new_string += new_letter
+            else:
+                new_string += letter
+        else:
+            if letter in "אבגדהוזחטיכלמנסעפצקרשתךםןףץ":
+                setting = "jib"
+                if letter in heb_to_jib:
+                    new_letter = heb_to_jib[letter]
+                    new_string += new_letter
+                else:
+                    new_string += letter
+            elif letter in "àáâãäåæçèéëìîðñòôö÷øùúêíïóõ":
+                setting = "heb"
+                if letter in jib_to_heb:
+                    new_letter = jib_to_heb[letter]
+                    new_string += new_letter
+                else:
+                    new_string += letter
+            else:
+                new_string += letter
+
     return new_string
 
 
 def main():
-    string = "þþàìáåí ìà éãåò"
+    string = "àìáåí ìà éãåò & - 3"
     print(jibrish_to_hebrew(string))
+
 
 if __name__ == '__main__':
     main()
-    
-
