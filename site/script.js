e4d7804c585893514fc751404c804c5e7c23ec66
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2023-06-06 12:52:30 +0300

    עדכון עיצוב וחלוקה יעילה

---
+++ site/script.js
@@ -0,0 +1,67 @@
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var searchInput = document.getElementById('searchInput').value.toLowerCase();
+  var searchBy = document.getElementById('searchBy').value;
+  searchSongs(searchInput, searchBy);
+});
+
+function searchSongs(query, searchBy) {
+  fetch('songs.csv')
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(csvText) {
+      var songs = parseCSV(csvText);
+      var results = filterSongs(songs, query, searchBy);
+      displayResults(results);
+    });
+}
+
+function parseCSV(csvText) {
+  var lines = csvText.split('\n');
+  var songs = [];
+  for (var i = 1; i < lines.length; i++) {
+    var line = lines[i].trim();
+    if (line) {
+      var columns = line.split(',');
+      var song = {
+        serial: columns[0],
+        name: columns[1],
+        album: columns[2],
+        singer: columns[3]
+      };
+      songs.push(song);
+    }
+  }
+  return songs;
+}
+
+function filterSongs(songs, query, searchBy) {
+  return songs.filter(function(song) {
+    var value = song[searchBy].toLocaleLowerCase();
+    return value.includes(query.toLocaleLowerCase());
+  });
+}
+
+
+function displayResults(results) {
+  var tableBody = document.querySelector('#resultsTable tbody');
+  tableBody.innerHTML = '';
+  for (var i = 0; i < results.length; i++) {
+    var song = results[i];
+    var row = document.createElement('tr');
+    var serialCell = document.createElement('td');
+    var nameCell = document.createElement('td');
+    var albumCell = document.createElement('td');
+    var singerCell = document.createElement('td');
+    serialCell.textContent = song.serial;
+    nameCell.textContent = song.name;
+    albumCell.textContent = song.album;
+    singerCell.textContent = song.singer;
+    row.appendChild(serialCell);
+    row.appendChild(nameCell);
+    row.appendChild(albumCell);
+    row.appendChild(singerCell);
+    tableBody.appendChild(row);
+  }
+}
