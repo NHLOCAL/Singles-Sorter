a0b4100787df80d1a05c34d5cd98fc1d4f9c9f28
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2023-06-06 18:54:20 +0300

    Revert "העברה למאגר אחר"
    This reverts commit d84ceec6bdefeaf07b580a7759240184ff6636d5.

---
+++ site/script.js
@@ -0,0 +1,74 @@
+document.getElementById('searchForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+  var searchInput = document.getElementById('searchInput').value.toLowerCase();
+  var searchBy = document.getElementById('searchBy').value;
+  searchSongs(searchInput, searchBy);
+});
+
+function searchSongs(query, searchBy) {
+  fetch('songs.csv')
+    .then(function(response) {
+      return response.text();
+    })
+    .then(function(csvText) {
+      var songs = parseCSV(csvText);
+      var results = filterSongs(songs, query, searchBy);
+      displayResults(results);
+    });
+}
+
+function parseCSV(csvText) {
+  var lines = csvText.split('\n');
+  var songs = [];
+  for (var i = 1; i < lines.length; i++) {
+    var line = lines[i].trim();
+    if (line) {
+      var columns = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
+      var song = {
+        serial: columns[0].trim(),
+        name: columns[1].trim(),
+        album: columns[2].trim(),
+        singer: columns[3].trim()
+      };
+      songs.push(song);
+    }
+  }
+  return songs;
+}
+
+
+
+function filterSongs(songs, query, searchBy) {
+  return songs.filter(function(song) {
+    var value = song[searchBy];
+    if (value) {
+      value = value.toLocaleLowerCase();
+      return value.includes(query.toLocaleLowerCase());
+    }
+    return false;
+  });
+}
+
+
+
+function displayResults(results) {
+  var tableBody = document.querySelector('#resultsTable tbody');
+  tableBody.innerHTML = '';
+  for (var i = 0; i < results.length; i++) {
+    var song = results[i];
+    var row = document.createElement('tr');
+    var serialCell = document.createElement('td');
+    var nameCell = document.createElement('td');
+    var albumCell = document.createElement('td');
+    var singerCell = document.createElement('td');
+    serialCell.textContent = song.serial;
+    nameCell.textContent = song.name;
+    albumCell.textContent = song.album;
+    singerCell.textContent = song.singer;
+    row.appendChild(serialCell);
+    row.appendChild(nameCell);
+    row.appendChild(albumCell);
+    row.appendChild(singerCell);
+    tableBody.appendChild(row);
+  }
+}
