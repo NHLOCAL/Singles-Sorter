6dc8ee28c7f1cb8dfdd6a95313d0cc67c59b5391
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-03-15 08:08:43 +0200

    Update try_model.py

--- machine-learn/try_model.py
+++ machine-learn/try_model.py
@@ -2,7 +2,7 @@ from spacy import load
 import sys
 
 # Load your trained model
-nlp = load("custom_ner_model")
+nlp = load("custom_ner_model2")
 
 def machine_learn(text):
     # Process the text with the loaded model
