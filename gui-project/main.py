2c42d9583349693f7938ea7a6b924b4d6e6eaa7a
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-05-08 21:05:23 +0300

    שיפורי עיצוב ומבנה

--- gui-project/main.py
+++ gui-project/main.py
@@ -4,14 +4,19 @@ from singles_sorter_func import scan_dir
 def main(page: ft.Page):
     page.title = "מסדר הסינגלים"
     page.vertical_alignment = ft.MainAxisAlignment.SPACE_BETWEEN
-    page.window_height = 680
-    page.window_width = 800
+    page.theme = ft.theme.Theme(color_scheme_seed="#FCD41C")
+    page.theme_mode = ft.ThemeMode.LIGHT
+    page.rtl=True
+    #page.window_height = 680
+    #page.window_width = 800
     
     
     
     LABLEL_PROGRAM = ft.Container(
         content=ft.Row(
             [
+                ft.Image(src="assets/icon.png", width=80, height=80),
+
                 ft.Text(
                     "מסדר הסינגלים 13.0",
                     size=40,
@@ -20,7 +25,7 @@ def main(page: ft.Page):
                     weight=ft.FontWeight.BOLD,
                     rtl=True,
                 ),
-                ft.Image(src="assets/icon.png", width=80, height=80),  # הוספת התמונה
+                
             ],
             alignment=ft.MainAxisAlignment.CENTER,
             
@@ -34,19 +39,22 @@ def main(page: ft.Page):
     
 
     # Input fields
-    source_dir_input = ft.TextField(label="תיקית הסינגלים שלך", autofocus=True, rtl=True, expand=True)
-    source_dir_button = ft.ElevatedButton("בחר תיקיה", on_click=lambda _: pick_directory(source_dir_input))
+    round_button = ft.ButtonStyle(shape=ft.RoundedRectangleBorder(radius=15))
+    height_button = '60'
+    round_text_field = ft.border_radius.only(15, 10, 15, 10)
 
-    target_dir_input = ft.TextField(label="תיקית יעד", rtl=True, expand=True)
-    target_dir_button = ft.ElevatedButton("בחר תיקיה", on_click=lambda _: pick_directory(target_dir_input))
+    source_dir_input = ft.TextField(label="תיקית הסינגלים שלך", autofocus=True, rtl=True, expand=True, border_radius=round_text_field, height='70')
+
+    target_dir_input = ft.TextField(label="תיקית יעד", rtl=True, expand=True,  border_radius=round_text_field, height='70')
     
     source_picker = ft.FilePicker(on_result=lambda e: update_path(e, source_dir_input))
     target_picker = ft.FilePicker(on_result=lambda e: update_path(e, target_dir_input))
     
-    page.overlay.extend([source_picker, target_picker])  # הוספת ה-FilePickers ל-overlay
+    page.overlay.extend([source_picker, target_picker])
+
     
-    source_dir_button = ft.ElevatedButton("בחר תיקיה", icon=ft.icons.FOLDER_OPEN, on_click=lambda _: source_picker.get_directory_path())
-    target_dir_button = ft.ElevatedButton("בחר תיקיה", icon=ft.icons.FOLDER_OPEN, on_click=lambda _: target_picker.get_directory_path())
+    source_dir_button = ft.ElevatedButton("בחר תיקיה", icon=ft.icons.FOLDER_OPEN, on_click=lambda _: source_picker.get_directory_path(), height=height_button, tooltip='בחירת תיקיה המכילה את המוזיקה שברצונך לסדר', style=round_button,)
+    target_dir_button = ft.ElevatedButton("בחר תיקיה", icon=ft.icons.FOLDER_OPEN, on_click=lambda _: target_picker.get_directory_path(), height=height_button, tooltip='בחירת תיקית יעד אליה יוכנסו תיקיות  המוזיקה שיווצרו', style=round_button)
 
 
     # Checkboxes
@@ -57,6 +65,7 @@ def main(page: ft.Page):
     abc_sort_checkbox = ft.Checkbox(label="צור תיקיות ראשיות לפי ה-א' ב'")
 
     # Progress bar
+    page.window_progress_bar='0.0'
     progress_bar = ft.ProgressBar(width=400, value=0)
 
     # Output text
@@ -80,12 +89,15 @@ def main(page: ft.Page):
         abc_sort = abc_sort_checkbox.value
 
         def progress_callback(progress):
-            progress_bar.value = progress / 100
+            progress_num = progress / 100
+            progress_bar.value = progress_num
+            page.window_progress_bar = str(progress_num)
             page.update()
 
         # Call the scan_dir function with arguments and progress callback
         scan_dir(source_dir, target_dir, copy_mode, abc_sort, exist_only, singles_folder, tree_folders, progress_callback)
         output_text.value = "מיון הקבצים הסתיים!"
+        page.window_progress_bar = '0.0'
         page.update()
 
     # Organize button
@@ -94,41 +106,52 @@ def main(page: ft.Page):
 
     page.add(
    
-        ft.Column(
-            [  
-                ft.Row([source_dir_button, source_dir_input], alignment=ft.MainAxisAlignment.CENTER),
-                ft.Row([target_dir_button, target_dir_input], alignment=ft.MainAxisAlignment.CENTER),
-            ],
+        ft.Container(
+            content=ft.Column(
+                [  
+                    ft.Row([source_dir_button, source_dir_input], alignment=ft.MainAxisAlignment.CENTER),
+                    ft.Row([target_dir_button, target_dir_input], alignment=ft.MainAxisAlignment.CENTER),
+                ],
+    
+                spacing='30',
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+
             
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            margin = ft.margin.all(20)
+
         ),
 
-        ft.Column(
-            [ 
-                ft.Column(
-                    [
-                    
-                    ft.Text("הגדרות בסיסיות"),
-                    
-                    copy_mode_checkbox,
-                    tree_folders_checkbox,
-                    
-                    ft.Text("מתקדם"),
-                    
-                    singles_folder_checkbox,
-                    exist_only_checkbox,
-                    abc_sort_checkbox],
-                    
-                    alignment=ft.MainAxisAlignment.CENTER,
-                    rtl=True,
-                ),
-                    progress_bar,
-                    output_text,
-                    organize_button,
-            ],
-            alignment=ft.MainAxisAlignment.CENTER,
-            horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+        ft.Container(
+            content=ft.Column(
+                [ 
+                    ft.Column(
+                        [
+                        
+                        ft.Text("הגדרות בסיסיות"),
+                        
+                        copy_mode_checkbox,
+                        tree_folders_checkbox,
+                        
+                        ft.Text("מתקדם"),
+                        
+                        singles_folder_checkbox,
+                        exist_only_checkbox,
+                        abc_sort_checkbox],
+                        
+                        alignment=ft.MainAxisAlignment.CENTER,
+                        rtl=True,
+                    ),
+                        progress_bar,
+                        output_text,
+                        organize_button,
+                ],
+                alignment=ft.MainAxisAlignment.CENTER,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+
+            margin = ft.margin.all(20)
         )
     )
     
