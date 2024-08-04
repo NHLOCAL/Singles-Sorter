d61a18ca0e0bd004150e7c39cb86cd4d75066f7d
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-08-04 06:44:56 +0300

    Update try1.py

--- src/core/try1.py
+++ src/core/try1.py
@@ -1,43 +1,33 @@
 import flet as ft
 
 def main(page: ft.Page):
-    def regular_action(e):
-        # קוד להפעלה רגילה
-        pass
-
-    def clean_only_action(e):
-        # קוד להפעלת ניקוי בלבד
-        pass
-
-    tabs = ft.Tabs(
-        selected_index=0,
-        tabs=[
-            ft.Tab(text="הפעל", content=ft.Text("הפעל",)),
-            ft.Tab(text="הפעל ניקוי בלבד", content=ft.Text("הפעל ניקוי בלבד",))
-        ]
+    page.add(
+        ft.AutofillGroup(
+            ft.Column(
+                controls=[
+                    ft.TextField(
+                        label="Name",
+                        autofill_hints=ft.AutofillHint.NAME,
+                    ),
+                    ft.TextField(
+                        label="Email",
+                        autofill_hints=[ft.AutofillHint.EMAIL],
+                    ),
+                    ft.TextField(
+                        label="Phone Number",
+                        autofill_hints=[ft.AutofillHint.TELEPHONE_NUMBER],
+                    ),
+                    ft.TextField(
+                        label="Street Address",
+                        autofill_hints=ft.AutofillHint.FULL_STREET_ADDRESS,
+                    ),
+                    ft.TextField(
+                        label="Postal Code",
+                        autofill_hints=ft.AutofillHint.POSTAL_CODE,
+                    ),
+                ]
+            )
+        )
     )
-    
-    
-    
-    # strings
-    page.client_storage.set("key", "value")
 
-    # numbers, booleans
-    page.client_storage.set("number.setting", 12345)
-    page.client_storage.set("bool_setting", True)
-
-    # lists
-    page.client_storage.set("favorite_colors", ["red", "green", "blue"])
-
-
-    # The value is automatically converted back to the original type
-    value = page.client_storage.get("key")
-
-    colors = page.client_storage.get("favorite_colors")
-    # colors = ["red", "green", "blue"]
-    print(colors)
-
-    
-    page.add(tabs)
-
-ft.app(target=main)
+ft.app(main)
