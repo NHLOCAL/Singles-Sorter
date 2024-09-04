c3d309e8233b1fcca20505a634a6377fbe13491a
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-09-05 00:56:44 +0300

    Update build_data.bat

--- machine-learn/scrape_data/build_data.bat
+++ machine-learn/scrape_data/build_data.bat
@@ -1,37 +1,50 @@
 @echo off
 
 echo.
+time /T
 echo #1 - duplic singers removing
+echo.
 
 cd level1
 
 "1-remove-duplic-singers.py"
 
 echo.
+time /T
 echo #2 song list processing
+echo.
 
 "2-song-list-processor.py"
 
 echo.
+time /T
 echo #3 - singers randoming
+echo.
 
 "3-randomer_singers.py"
 
 echo.
+time /T
 echo #4 - creat json
+echo.
 
 cd..
 
 "1-creat_json.py"
 
 echo.
+time /T
 echo #5 - cleaner json
+echo.
 
 "2-cleaner_json.py"
 
 echo.
-echo #6 - pull to git
+time /T
+echo Operation completed successfully!
+echo.
 
+rem  #6 - pull to git
 rem git pull
 
 pause
