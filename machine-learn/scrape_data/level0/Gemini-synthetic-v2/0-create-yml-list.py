e1050e6467c452cc99a85baba02cd80d2770119c
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-12-19 14:43:59 +0200

    יצירת תגים עבור שורות 60000-סוף

--- machine-learn/scrape_data/level0/Gemini-synthetic-v2/0-create-yml-list.py
+++ machine-learn/scrape_data/level0/Gemini-synthetic-v2/0-create-yml-list.py
@@ -40,4 +40,4 @@ def generate_jobs(start, end, step, output_file):
         f.write("\n".join(jobs))
 
 # Generate jobs for ranges 1000 to 5000 with a step of 1000
-generate_jobs(50001, 60000, 2000, 'jobs_output.txt')
+generate_jobs(60001, 73370, 2000, 'jobs_output.txt')
