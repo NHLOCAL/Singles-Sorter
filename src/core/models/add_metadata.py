5072c8ec4312c4a641c459e187eda3c7742432b8
NHLOCAL <NH.LOCAL11@GMAIL.COM>
2024-12-19 02:49:31 +0200

    עדכון מטאדאטה מודלי AI

--- src/core/models/add_metadata.py
+++ src/core/models/add_metadata.py
@@ -5,7 +5,7 @@ nlp = spacy.load("singer_ner_he")
 
 # עדכון המטא נתונים
 nlp.meta['name'] = 'singer_ner_he'
-nlp.meta['version'] = '0.23.0'
+nlp.meta['version'] = '1.0.0'
 nlp.meta['description'] = 'Model for recognizing singer names in Hebrew song titles'
 nlp.meta['author'] = 'nhlocal'
 nlp.meta['email'] = 'nh.local11@gmail.com'
